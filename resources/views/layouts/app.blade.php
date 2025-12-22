6c5d913d9acddffa0edcd8526d59424a611eb10e
BrazD <brazdanarantes@gmail.com>
2025-12-22 16:02:04 -0300

    Cria layout base com Blade (app.blade.php)

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Task Manager'</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+    <header>
+        <h1>Task Manager</h1>
+        <nav>
+            <a href="{{ route('tasks.index') }}"></a>
+        </nav>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <small>Projeto de estudo com Laravel</small>
+    </footer>
+    
+</body>
+</html>
